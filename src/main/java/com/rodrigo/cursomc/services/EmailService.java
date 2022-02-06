f7311dfe4f6a7530982c802de6aaac2ada338f6b
BrRodrigo1504 <rodrigops1504@gmail.com>
2022-02-06 23:50:24 +0000

    MockEmailService com Logger. Padroes Strategy e Template Method

---
+++ src/main/java/com/rodrigo/cursomc/services/EmailService.java
@@ -0,0 +1,12 @@
+package com.rodrigo.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.rodrigo.cursomc.domain.Pedido;
+
+public interface EmailService {
+
+	void sendOrderConfirmationEmail(Pedido obj);
+	
+	void sendEmail(SimpleMailMessage msg);
+}
