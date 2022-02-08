cd3c59b685de9c1d88c90b1f7b8710ece3bf1e83
BrRodrigo1504 <rodrigops1504@gmail.com>
2022-02-08 23:18:46 +0000

    Restrição de Conteúdos: Cliente só Recupera seus Pedidos

--- src/main/java/com/rodrigo/cursomc/services/PedidoService.java
+++ src/main/java/com/rodrigo/cursomc/services/PedidoService.java
@@ -1,14 +1,17 @@
 package com.rodrigo.cursomc.services;
 
 import java.util.Date;
-
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.rodrigo.cursomc.domain.Cliente;
 import com.rodrigo.cursomc.domain.ItemPedido;
 import com.rodrigo.cursomc.domain.PagamentoComBoleto;
 import com.rodrigo.cursomc.domain.Pedido;
@@ -16,6 +19,8 @@ import com.rodrigo.cursomc.domain.enums.EstadoPagamento;
 import com.rodrigo.cursomc.repositories.ItemPedidoRepository;
 import com.rodrigo.cursomc.repositories.PagamentoRepository;
 import com.rodrigo.cursomc.repositories.PedidoRepository;
+import com.rodrigo.cursomc.security.UserSS;
+import com.rodrigo.cursomc.services.exceptions.AuthorizationException;
 import com.rodrigo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -38,7 +43,7 @@ public class PedidoService {
 
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@Autowired
 	private EmailService emailService;
 
@@ -47,6 +52,7 @@ public class PedidoService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
+
 	@Transactional
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
@@ -69,5 +75,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
