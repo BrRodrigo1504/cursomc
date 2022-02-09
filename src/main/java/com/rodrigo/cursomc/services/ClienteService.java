b77cc809f3e97f80a1e59950d9568a483173d208
BrRodrigo1504 <rodrigops1504@gmail.com>
2022-02-09 19:59:00 +0000

    Enviando Arquivo via EndPoint

--- src/main/java/com/rodrigo/cursomc/services/ClienteService.java
+++ src/main/java/com/rodrigo/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.rodrigo.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.rodrigo.cursomc.domain.Cidade;
 import com.rodrigo.cursomc.domain.Cliente;
@@ -32,6 +34,9 @@ public class ClienteService {
 
 	@Autowired
 	private BCryptPasswordEncoder pe;
+	
+	@Autowired
+ 	private S3Service s3Service;
 	
 	@Autowired
 	private ClienteRepository repo;
@@ -109,4 +114,8 @@ public class ClienteService {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+ 		return s3Service.uploadFile(multipartFile);
+ 	}
 }
